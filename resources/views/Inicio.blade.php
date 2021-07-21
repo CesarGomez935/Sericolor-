6c715c6d12c86bacf7b9886bc36e1408d68e3e0b
CesarGomez935 <87777599+CesarGomez935@users.noreply.github.com>
2021-07-21 14:13:09 -0600

    Primer plantilla

---
+++ resources/views/Inicio.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sericolor</title>
+
+     <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.1/dist/css/uikit.min.css" />
+
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.7.1/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.7.1/dist/js/uikit-icons.min.js"></script>
+
+</head>
+
+<header>
+    
+</header>
+<body  >
+
+
+<div uk-margin-right class="uk-position-right uk-padding">
+
+    <div uk-margin-right class="uk-padding" >
+
+        <button class="uk-button uk-button-default uk-button-large">Revisa tu pedido</button>
+        
+    </div>  
+
+    <div uk-margin-right class=".uk-margin-left uk-padding" >
+
+        <button class="uk-button uk-button-default uk-button-small ">Inicio</button>
+        <button class="uk-button uk-button-default uk-button-small">Servicios</button>
+        <button class="uk-button uk-button-default uk-button-small">Acerca de</button>
+        <button class="uk-button uk-button-default uk-button-small">Contáctanos</button>
+    </div> 
+    
+</div>
+
+<div uk-margin class=".uk-margin-left uk-padding">
+
+        <img uk-margin data-src="/img/Logo_sericolor.png" width="150" height="1200" alt="" uk-img>  
+    
+</div>
+
+
+
+</body>
+</html>
