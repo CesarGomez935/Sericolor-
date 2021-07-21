e183229641e65d3dd22af4fa81a3099c343140c1
CesarGomez935 <87777599+CesarGomez935@users.noreply.github.com>
2021-07-21 14:59:08 -0600

    Update Inicio.blade.php

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -47,6 +47,88 @@
 </div>
 
 
+<div class="uk-padding">
+<div class="uk-grid-medium uk-child-width-expand@s uk-text-center" uk-grid>
+    <div>
+    <img uk-margin data-src="/img/Logo_sericolor.png" width="150" height="1200" alt="" uk-img>
+    </div>
+    <div>
+    <img uk-margin data-src="/img/Logo_sericolor.png" width="150" height="1200" alt="" uk-img>
+    </div>
+    <div>
+    <img uk-margin data-src="/img/Logo_sericolor.png" width="150" height="1200" alt="" uk-img>
+    </div>
+    <div>
+    <img uk-margin data-src="/img/Logo_sericolor.png" width="150" height="1200" alt="" uk-img>
+    </div>
+    
+</div>
+<div class="uk-grid-medium uk-child-width-expand@s uk-text-center" uk-grid>
+
+    <div>
+        <button class="uk-button uk-button-primary uk-button-small ">Bordado</button>
+    </div>
+
+    <div>
+        <button class="uk-button uk-button-primary uk-button-small ">Sublimación</button>
+    </div>
+
+    <div>
+        <button class="uk-button uk-button-primary uk-button-small ">Serigrafía</button>
+    </div>
+
+    <div>
+        <button class="uk-button uk-button-primary uk-button-small ">Impresión digital</button>
+    </div>
+    
+</div>
+</div>
+
+
+
 
 </body>
+<footer style="background-color: #AEC6CF">
+
+<div class="uk-grid-divider uk-child-width-expand@s uk-padding" uk-grid>
+    <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.6598222185626!2d-85.92170308581036!3d12.929572019298973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f7249d2987e4149%3A0x1e72bbacc5e03499!2sSERICOLOR%20PUBLICIDAD!5e0!3m2!1ses!2sni!4v1626899274980!5m2!1ses!2sni" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
+    <div>
+        <p>
+            <B> Ubicados frente a donde fue metales de matagalpa
+            
+           
+            </B>           
+        </p>
+
+        <p>
+            <B> 
+            
+            Telefono: 2773-2011
+            Whatsapp: 5504-8894 
+
+            
+            </B>           
+        </p>
+        <p>
+            <B> 
+
+            Todos los derechos reservados
+            </B>           
+        </p>
+    </div>
+    <div>
+            <a href="https://www.instagram.com/sericolorpublicidad/?hl=es-la" uk-icon="icon: instagram ; ratio: 3.5"></a>
+            <a href="https://es-la.facebook.com/pg/sericolorpublicidad/services/?service_id=1800097760043283" uk-icon="icon: facebook ; ratio: 3.5"></a>
+    </div>
+</div>
+
+
+    
+
+
+
+
+
+
+</footer>
 </html>
