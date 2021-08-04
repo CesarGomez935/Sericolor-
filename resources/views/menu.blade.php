f1bdace6ea8fda8954efb2531c81b77c3847c4ad
CesarGomez935 <87777599+CesarGomez935@users.noreply.github.com>
2021-08-04 14:45:39 -0600

    Hiper vinculos menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -40,7 +40,8 @@
             <table class="uk-table uk-padding  uk-margin">
                 <tbody>
                     <tr>
-                        <td style="padding-left: 100px;"><button class="btn btn-lg uk-padding" style="background-color:transparent">
+                        <td style="padding-left: 100px;">
+                            <a href="/menu_facturacion" class="btn btn-lg uk-padding" style="background-color:transparent">
                                 <div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z" />
                                         <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z" />
@@ -48,49 +49,54 @@
                                         <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z" />
                                     </svg></div>
                                 Facturación
-                            </button></td>
-                        <td style="padding-left: 100px;"><button class="btn btn-lg uk-padding " style="background-color:transparent">
+                            </a>
+                        </td>
+                        <td style="padding-left: 100px;">
+                            <a href="/pedidos_sublimacion" class="btn btn-lg uk-padding " style="background-color:transparent">
                                 <div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-droplet-half " viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10c0 0 2.5 1.5 5 .5s5-.5 5-.5c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z" />
                                         <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448z" />
                                     </svg></div>
                                 Pedidos de Sublimación
-                            </button></td>
+                            </a>
+                        </td>
                         <td style="padding-left: 100px;">
-                            <button class="btn btn-lg" style="background-color:transparent">
+                            <a href="/pedidos_impresion_digital" class="btn btn-lg" style="background-color:transparent">
                                 <div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
                                         <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
                                         <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
                                     </svg></div>
                                 Pedidos de Impresión Digital
-                            </button>
+                            </a>
                         </td>
                     </tr>
                     <tr>
-                        <td style="padding-left: 100px;"> <button class="btn btn-lg" style="background-color:transparent">
+                        <td style="padding-left: 100px;"> 
+                            <a href="/pedidos_serigrafia" class="btn btn-lg" style="background-color:transparent">
                                 <div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-eyedropper" viewBox="0 0 16 16">
                                         <path d="M13.354.646a1.207 1.207 0 0 0-1.708 0L8.5 3.793l-.646-.647a.5.5 0 1 0-.708.708L8.293 5l-7.147 7.146A.5.5 0 0 0 1 12.5v1.793l-.854.853a.5.5 0 1 0 .708.707L1.707 15H3.5a.5.5 0 0 0 .354-.146L11 7.707l1.146 1.147a.5.5 0 0 0 .708-.708l-.647-.646 3.147-3.146a1.207 1.207 0 0 0 0-1.708l-2-2zM2 12.707l7-7L10.293 7l-7 7H2v-1.293z" />
                                     </svg></div>
                                 Pedidos de Serigrafía
-                            </button></td>
+                            </a>
+                        </td>
                         <td style="padding-left: 100px;">
-                            <button class="btn btn-lg" style="background-color:transparent">
+                            <a href="/pedidos_bordado" class="btn btn-lg" style="background-color:transparent">
                                 <div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-vector-pen" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M10.646.646a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1 0 .708l-1.902 1.902-.829 3.313a1.5 1.5 0 0 1-1.024 1.073L1.254 14.746 4.358 4.4A1.5 1.5 0 0 1 5.43 3.377l3.313-.828L10.646.646zm-1.8 2.908-3.173.793a.5.5 0 0 0-.358.342l-2.57 8.565 8.567-2.57a.5.5 0 0 0 .34-.357l.794-3.174-3.6-3.6z" />
                                         <path fill-rule="evenodd" d="M2.832 13.228 8 9a1 1 0 1 0-1-1l-4.228 5.168-.026.086.086-.026z" />
                                     </svg></div>
                                 Pedidos de Bordado
-                            </button>
+                            </a>
                         </td>
                         <td style="padding-left: 100px;">
-                            <button class="btn btn-lg" style="background-color:transparent">
+                            <a href="/menuadmon" class="btn btn-lg" style="background-color:transparent">
                                 <div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
                                         <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
                                         <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z" />
                                         <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
                                     </svg></div>
-                                Pedidos de Bordado
-                            </button>
+                                Administración
+                            </a>
                         </td>
                     </tr>
 
@@ -99,9 +105,9 @@
         </form>
     </div>
     
-    <div class="uk-padding uk-background-muted uk-padding  ">
+    <div class="uk-padding uk-background-muted uk-padding ">
         <div class="uk-div uk-margin position-relative .uk-padding-large" style="text-align: center;">
-            <button class="uk-button uk-button-primary "style="">Atrás</button>
+            <a href="/Inicio" class="uk-button uk-button-primary "style="">Salir</a>
         </div>
     </div>
 </body>
