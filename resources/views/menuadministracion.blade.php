508698ffdad7b1f99fa5139e6fa655544430ee51
CesarGomez935 <87777599+CesarGomez935@users.noreply.github.com>
2021-08-04 14:51:53 -0600

    Update menuadministracion.blade.php

--- resources/views/menuadministracion.blade.php
+++ resources/views/menuadministracion.blade.php
@@ -77,7 +77,7 @@
                         </a>
                     </td>
                     <td style="padding-left: 100px;">
-                        <a href="/clientes" class="btn btn-lg" style="background-color:transparent">
+                        <a href="/cliente" class="btn btn-lg" style="background-color:transparent">
                             <div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-person-badge-fill" viewBox="0 0 16 16">
                                     <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z" />
                                 </svg></div>
