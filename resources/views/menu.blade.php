cb71ff288a4a6c0639454ee07e8ab3e4839a8f14
tobiluisdark <66345137+tobiluisdark@users.noreply.github.com>
2021-08-18 23:03:22 -0600

    Update menu.blade.php

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -10,7 +10,32 @@
     <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.1/dist/js/uikit-icons.min.js"></script>
    
 </head>
+<div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+        <!-- Copyright -->
+        <div class="text-white mb-3 mb-md-0">
+        Yo soy el buen pastor, y conozco mis ovejas y las mías me conocen,
 
+        </div>
+        <!-- Copyright -->
+
+        <!-- Right -->
+        <div>
+            <a href="https://www.biblegateway.com/passage/?search=Juan%2010%3A14&version=RVR1960" class="text-white me-4">
+                <i class="fab fa-facebook-f"></i>
+                Juan 10:14
+            </a>
+            <a href="#!" class="text-white me-4">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#!" class="text-white me-4">
+                <i class="fab fa-google"></i>
+            </a>
+            <a href="#!" class="text-white">
+                <i class="fab fa-linkedin-in"></i>
+            </a>
+        </div>
+        <!-- Right -->
+    </div>
 <body>
     <div style="text-align: center;">
         <form class="uk-padding">
