3b21b3da0fd5d143b2eadad827de4ce4f480a61f
CesarGomez935 <87777599+CesarGomez935@users.noreply.github.com>
2021-08-20 14:58:21 -0600

    Nueva interfazz

--- resources/views/Inicio2.blade.php
+++ resources/views/Inicio2.blade.php
@@ -49,36 +49,119 @@
 </header>
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #d9d9d9;">
 
-          <img src="\img\Sericolor_ Logo.png" width="200" alt="">
+          <img src="\img\Sericolor_ Logo.png" width="250px"  class="img-fluid">
+        </div>
           <div class="navbar-collapse collapse" ></div>
-          <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler collapsed " type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="navbar-collapse collapse" id="navbarColor02" style="">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-3 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="#"><b> Inicio </b></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#"> <b> Conocenos </b></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link" href="#"><b> Catálogo </b></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link" href="#"><b> Galería </b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><b> Contacto</b></a>
               </li>
             </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-light" type="submit">Search</button>
-            </form>
+
           </div>
         </div>
-      </nav>
+    </nav>
+
+
+    <div class="mx-auto" >
+
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/img/Ima 1.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block " style="background-color: #8096dfa3">
+                    <h5 class="fs-3"><b> Aceca de Nosotros</b></h5>
+                    <p class="fs-5"><b> Con 8 años de experiencia en el mercado de la industria gráfica en Matagalpa, ofrecemos servicios de impresión, digital pequeño formato, digital gran formato, bordados, sublimación y diseño gráfico, imprimimos desde una tarjeta de presentación hasta una valla publicitaria, todo en un solo lugar. </b></p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/img/Ima 2.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/img/Ima 3.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+
+        </div>
+
+            <img src="\img\Barrita.jpg" class="" alt="...">
+
+
+
+    </div>
+    <div class="container">
+        <div class="m-1 p-3">
+        <h3 style="text-align: center">Nuestros servicios</h3>
+        </div>
+        <div class="row">
+          <div class="col">
+            <a href="/servicio#serigrafia">
+                <img  src="/img/Ser.png" alt="">
+              </a>
+          </div>
+          <div class="col">
+            <a href="/servicio#impresion_digital">
+                <img  src="/img/Imp.png" alt="">
+              </a>
+          </div>
+          <div class="col">
+            <a href="/servicio#sublimacion">
+                <img  src="/img/Sub.png" alt="">
+              </a>
+          </div>
+          <div class="col">
+              <a href="/servicio#bordado">
+                <img  src="/img/Bor.png" alt="">
+              </a>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
 
 
 </body>
