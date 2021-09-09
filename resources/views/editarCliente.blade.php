75cb18285cb124f37ec9683403c5c21527b27b7a
CesarGomez935 <87777599+CesarGomez935@users.noreply.github.com>
2021-09-09 09:03:47 -0600

    Update editarCliente.blade.php

--- resources/views/editarCliente.blade.php
+++ resources/views/editarCliente.blade.php
@@ -23,7 +23,8 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 </head>
 
-<body>
+<body onpageshow="cambiarselect();">
+
 
     <form action="{{route("cliente.update",$cliente)}}" method="POST">
 
@@ -193,60 +194,35 @@
 
 </body>
 <script>
-    let detallesdepedido = [];
+    function cambiarselect() {
 
-    cargarcliente();
+        var val = "{{$cliente->tipodepersona}}";
 
-    $('#guardar').click(function(res) {
-        guardarpedido();
-        alert("Se agrego correctamente al cliente");
+        // document.querySelector('#rol_asignado_trabajador [value="' + val + '"]').selected = true;
 
-    });
+        $('#tipo_cliente').val(val).change();
 
-    function peticionapi(data, method, onSucess) {
-        let url = '/api/cliente';
-        if (method == 'PUT' || method == 'DELETE') {
-            url += '/' + data.IDpersona;
-        }
-        $.ajax({
-            url: url
-            , method: method
-            , data: data
-            , error(ext) {
-                let error = e.responseJSON.errors;
-                let msj = error[Object.keys(error)[0]][0];
-                alert(msj);
-            }
-            , success(res) {
 
-            }
-        })
-    }
 
-    function guardarpedido() {
-        let data = {
-            Rol: $("#Rol").val()
-            , primer_nombre: $("#primer_nombre_cliente").val()
-            , segundo_nombre: $("#segundo_nombre_cliente").val()
-            , primer_apellido: $("#primer_apellido_cliente").val()
-            , segundo_apellido: $("#segundo_apellido_cliente").val()
-            , tipodepersona: $("#tipo_cliente").val()
-            , telefono: $("#telefono_cliente").val()
-            , cedula: $("#cedula_cliente").val()
-        , };
-        peticionapi(data, 'POST', function(res) {
-            alert('Guardado con exito')
-        });
-    }
 
-    function cargarcliente() {
-        peticionapi({}, 'GET', function(res) {
-            console.log(res);
-            alert('respuesta satisfactoria');
-        });
+
+
+
+
+
+        // $('#rol_asignado_trabajador option:contains(' + val + ')').prop({
+        //     selected: true
+        // });
+
+
+
+
+
+
 
     }
 
 </script>
+
 
 </html>
