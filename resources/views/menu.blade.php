818b3e1e6652ac3b2a377bf92420c8e751ecbbda
CesarGomez935 <87777599+CesarGomez935@users.noreply.github.com>
2021-12-16 13:23:09 -0600

    ruta

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -63,7 +63,7 @@
                 <tbody>
                     <tr>
                         <td style="padding-left: 100px;">
-                            <a href="/menu/menu_facturacion" class="btn btn-lg uk-padding"
+                            <a href="/menu/facturacion" class="btn btn-lg uk-padding"
                                 style="background-color:transparent">
                                 <div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" width="200"
                                         height="200" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
@@ -138,18 +138,19 @@
                         </td>
                         <td style="padding-left: 100px;">
 
-                            @if (auth()->user()->Privilegios=='Administrador')
+                            @if (auth()->user()->Privilegios == 'Administrador')
                                 <a href="/menu/menuadmon" class="btn btn-lg" style="background-color:transparent">
-                                <div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" width="200"
-                                        height="200" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
-                                        <path
-                                            d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                                        <path fill-rule="evenodd"
-                                            d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z" />
-                                        <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
-                                    </svg></div>
-                                Administración
-                            </a>
+                                    <div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" width="200"
+                                            height="200" fill="currentColor" class="bi bi-people-fill"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                            <path fill-rule="evenodd"
+                                                d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z" />
+                                            <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
+                                        </svg></div>
+                                    Administración
+                                </a>
                             @endif
                         </td>
                     </tr>
