1740aa6661e52c7d737f561c67bb2dbc168dc990
CesarGomez935 <87777599+CesarGomez935@users.noreply.github.com>
2022-01-19 10:32:44 -0600

    Update ResumenPedidos.blade.php

--- resources/views/ResumenPedidos.blade.php
+++ resources/views/ResumenPedidos.blade.php
@@ -145,7 +145,7 @@
 
     <div class="uk-padding uk-background-muted uk-padding  ">
         <div class="uk-div uk-margin position-relative .uk-padding-large" style="text-align: center;">
-            <a href="/menu/menuadmon" class="uk-button uk-button-primary " style="margin-left: 100px">Atrás</a>
+            <a href="/menu" class="uk-button uk-button-primary " style="margin-left: 100px">Atrás</a>
             <button class="uk-button uk-button-secondary" style="margin-left: 100px">Guardar</button>
 
         </div>
